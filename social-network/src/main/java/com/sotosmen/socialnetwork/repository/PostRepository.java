617f91b9f2b885999f73d1f822a6c073b5ca5936
sotosmen <sotosmen@hotmail.com>
2020-03-09 22:15:29 +0200

    Version 0.2

--- social-network/src/main/java/com/sotosmen/socialnetwork/repository/PostRepository.java
+++ social-network/src/main/java/com/sotosmen/socialnetwork/repository/PostRepository.java
@@ -1,10 +1,14 @@
 package com.sotosmen.socialnetwork.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sotosmen.socialnetwork.post.Post;
 import com.sotosmen.socialnetwork.post.PostCompositeKey;
+import com.sotosmen.socialnetwork.thread.ThreadCompositeKey;
 
 public interface PostRepository extends JpaRepository<Post,PostCompositeKey> {
-	
+	List<Post> findByIdUserIdP(String username);
+	List<Post> findByIdThreadId(ThreadCompositeKey id);
 }
--- social-network/src/main/java/com/sotosmen/socialnetwork/strings/Strings.java
+++ social-network/src/main/java/com/sotosmen/socialnetwork/strings/Strings.java
@@ -1,6 +1,11 @@
 package com.sotosmen.socialnetwork.strings;
 
 public class Strings {
+	public static final String nullObj = "The object sent is empty.";
+	public static final String exPost = "Post with this id already exists.";
+	public static final String noPosts = "No posts were found.";
+	public static final String noPostsThread = "No posts were found in this thread.";
+	public static final String noPostsUser = "No posts were found for this user.";
 	public static final String noThreads = "No Threads were found.";
 	public static final String noThread = "No Thread with that name was found.";
 	public static final String threadEx = "Thread with that name already exists.";
@@ -9,5 +14,7 @@ public class Strings {
 	public static final String noUser = "No user with that name was found.";
 	public static final String noMessageUser = "No message found from that user.";
 	public static final String noMessageCon = "No messages found in this conversation.";
-	public static final String noMessages = "No messages found.";
+	public static final String noMessages = "No messages were found.";
+	public static final String noConversations = "No conversations were found.";
+	public static final String noConversationsUser = "No conversations were found for this user.";
 }
