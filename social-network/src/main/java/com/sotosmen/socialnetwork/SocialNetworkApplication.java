e1393bf6e3433b0f3a19e934d3e443d9cf8f6a19
sotosmen <sotosmen@hotmail.com>
2020-03-09 21:40:32 +0200

    Version 0.2

--- social-network/src/main/java/com/sotosmen/socialnetwork/SocialNetworkApplication.java
+++ social-network/src/main/java/com/sotosmen/socialnetwork/SocialNetworkApplication.java
@@ -2,18 +2,14 @@ package com.sotosmen.socialnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilterChain;
 
-import com.sotosmen.socialnetwork.amqp.DestinationsConfig;
-
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
-@EnableConfigurationProperties(DestinationsConfig.class)
 public class SocialNetworkApplication {
 	
 	public static void main(String[] args) {
